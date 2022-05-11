1a749ff41acd026d85f15a87502c6658a4f53720
Faeryn <autistic.batman@gmail.com>
2022-05-11 19:08:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# TravelSpeed
+
+Increases movement speed outside combat.  
+WARNING: The mod is in a very early (PoC) stage, untested and might break in unexpected ways. Use at your own risk.
+
+The movement speed multiplier is configurable, default is 2x.  
+It's strongly recommended to use Outward Config Manager.
+
+## Planned Features
+- Create a Planned Features list
+
+## Changelog
+
+### 0.1.0
+- Initial release
---
+++ plugin/TravelSpeed.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace TravelSpeed {
+	[BepInPlugin(GUID, NAME, VERSION)]
+	public class TravelSpeed : BaseUnityPlugin {
+		public const string GUID = "faeryn.travelspeed";
+		public const string NAME = "TravelSpeed";
+		public const string VERSION = "0.1.0";
+		private const string DISPLAY_NAME = "Travel Speed";
+		internal static ManualLogSource Log;
+
+		private float DefaultSpeed = 4.0f;
+		
+		public static ConfigEntry<float> TravelSpeedMult;
+
+		internal void Awake() {
+			Log = this.Logger;
+			Log.LogMessage($"Starting {NAME} {VERSION}");
+			InitializeConfig();
+		}
+
+		private void InitializeConfig() {
+			TravelSpeedMult = Config.Bind(DISPLAY_NAME, "Travel Speed Multiplier", 2f, "The character moves this many times faster than normal when out of combat");
+			TravelSpeedMult.SettingChanged += (sender, args) => {
+				UpdateTravelSpeed();
+			};
+		}
+
+		private void Update() {
+			UpdateTravelSpeed();
+		}
+
+		private void UpdateTravelSpeed() {
+			Character character = GetLocalCharacter(0);
+			if (character != null) {
+				UpdateTravelSpeed(character);
+			}
+		}
+
+		private void UpdateTravelSpeed(Character character) {
+			if (character.InCombat) {
+				if (!Mathf.Approximately(character.Speed, DefaultSpeed)) {
+					character.Speed = DefaultSpeed;
+				}
+			} else {
+				if (!Mathf.Approximately(character.Speed, DefaultSpeed * TravelSpeedMult.Value)) {
+					character.Speed = DefaultSpeed * TravelSpeedMult.Value;
+				}
+			}
+		}
+		
+		private Character GetLocalCharacter(int playerID) {
+			return SplitScreenManager.Instance.LocalPlayers[playerID].AssignedCharacter;
+		}
+	}
+}
